80deb1bc837fdeec8d4a9b9ea0faa49791cc1847
Francisco Javier Lledó Fuster <javilledo@gmail.com>
2023-09-04 07:07:14 +0200

    Agregar archivos de proyecto.

---
+++ MyAutoCADCommands/Initialitation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.Runtime;
+
+namespace MyAutoCADCommands
+{
+    internal class Initialitation: IExtensionApplication
+    {
+        [CommandMethod("MyFirstCommand")]
+        public void cmdMyFirstCommand()
+        {
+
+        }
+
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Terminate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
