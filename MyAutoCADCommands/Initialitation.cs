fb95098480649b0182cbd9f374d107bdbaed67fc
Francisco Javier Lledó Fuster <javilledo@gmail.com>
2023-09-09 09:26:45 +0200

    cmdLogin function added

--- MyAutoCADCommands/Initialitation.cs
+++ MyAutoCADCommands/Initialitation.cs
@@ -11,8 +11,8 @@ namespace MyAutoCADCommands
 {
     public class Initialitation: IExtensionApplication
     {
-        [CommandMethod("MyFirstCommand")]
-        public void cmdMyFirst()
+        #region Commands
+        [CommandMethod("MyFirstCommand")] public void cmdMyFirst()
         {
             Document doc = Application.DocumentManager.MdiActiveDocument;
             Editor ed = doc.Editor;
@@ -27,7 +27,51 @@ namespace MyAutoCADCommands
                 ed.WriteMessage("\nFile is a new drawing file");
             }
         }
+        
+        [CommandMethod("LIGetVersion")] public void cmdAcadVersion()
+        {
+            Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
 
+            switch(Application.DocumentManager.MdiActiveDocument.Database.LastSavedAsVersion)
+            {
+                case (Autodesk.AutoCAD.DatabaseServices.DwgVersion.AC1032):
+                    ed.WriteMessage("\nAutoCAD 2018");
+                    break;
+
+                case (Autodesk.AutoCAD.DatabaseServices.DwgVersion.AC1027):
+                    ed.WriteMessage("\nAutoCAD 2013");
+                    break;
+
+                case (Autodesk.AutoCAD.DatabaseServices.DwgVersion.AC1024):
+                    ed.WriteMessage("\nAutoCAD 2010");
+                    break;
+
+                case (Autodesk.AutoCAD.DatabaseServices.DwgVersion.AC1021):
+                    ed.WriteMessage("\nAutoCAD 2007");
+                    break;
+
+                default:
+                    ed.WriteMessage("\nPrior to AutoCAD 2007");
+                    break;
+
+            }
+        }
+
+        [CommandMethod("LIGetLoginName")] public void cmdLogin()
+        {
+            Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
+
+            string logName = System.Convert.ToString(Application.GetSystemVariable("LoginName"));
+            ed.WriteMessage("\n" +  logName);
+
+            int firstL = logName.IndexOf("l");
+            ed.WriteMessage("\n" + logName.Substring(0, firstL) + " " + logName.Substring(firstL));
+
+        }
+        
+        #endregion
+
+        #region SupportFunctions
         private Boolean IsSavedFile()
         {
             int dResult = System.Convert.ToInt16(Application.GetSystemVariable("DWGTITLED"));
@@ -42,6 +86,14 @@ namespace MyAutoCADCommands
             }
         }
 
+        private void AnotherConditionMethod()
+        {
+
+        }
+
+        #endregion
+
+        #region Initialization
         void IExtensionApplication.Initialize()
         {
 
@@ -51,5 +103,9 @@ namespace MyAutoCADCommands
         {
             
         }
+
+        #endregion
+
+
     }
 }
