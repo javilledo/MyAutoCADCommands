14791baa6e586be57691b56e93dfa8021514e51a
Francisco Javier Lledó Fuster <javilledo@gmail.com>
2023-09-09 07:18:48 +0200

    My First Command initialized!

--- MyAutoCADCommands/Initialitation.cs
+++ MyAutoCADCommands/Initialitation.cs
@@ -4,25 +4,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.ApplicationServices;
 
 namespace MyAutoCADCommands
 {
-    internal class Initialitation: IExtensionApplication
+    public class Initialitation: IExtensionApplication
     {
         [CommandMethod("MyFirstCommand")]
-        public void cmdMyFirstCommand()
+        public void cmdMyFirst()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Editor ed = doc.Editor;
+            //ed.WriteMessage("\nHello World!");
+
+            if(IsSavedFile() == true)
+            {
+                ed.WriteMessage("\nFile is an existing file");
+            }
+            else
+            {
+                ed.WriteMessage("\nFile is a new drawing file");
+            }
+        }
+
+        private Boolean IsSavedFile()
+        {
+            int dResult = System.Convert.ToInt16(Application.GetSystemVariable("DWGTITLED"));
+
+            if (dResult != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        void IExtensionApplication.Initialize()
         {
 
         }
 
-        public void Initialize()
+        void IExtensionApplication.Terminate()
         {
-            throw new NotImplementedException();
-        }
-
-        public void Terminate()
-        {
-            throw new NotImplementedException();
+            
         }
     }
 }
